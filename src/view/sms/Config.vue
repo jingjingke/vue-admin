d3b3298c23d5a9813932c9b55b8fd0b10afe8f9d
jingjingke <919948650@qq.com>
2017-12-08 19:53:46 +0800

    短信模板弹窗逻辑

--- src/view/sms/Config.vue
+++ src/view/sms/Config.vue
@@ -29,13 +29,13 @@
 				<el-table-column align='center' label="扩展字段" prop='senceExt' width='120' />
 				<el-table-column align='center' label="短信模版" prop='templateContent' min-width='360' />
 				<el-table-column align='center' label="状态" prop='status' :formatter='formatValue' width='100' />
-				<el-table-column align='center' label="操作" width='140px' >
+				<el-table-column align='center' label="操作" width='140px'>
 					<template slot-scope="scope">
 						<template v-if='scope.row.status === 1'>
 							<el-button size="mini" type="primary" icon="el-icon-edit" plain @click='openChange(scope.row)'>修改</el-button>
 						</template>
 						<template v-if='scope.row.status === 3'>
-							<el-button size="mini" type="danger" plain >查看不过理由</el-button>
+							<el-button size="mini" type="danger" plain @click='openReason(scope.row)'>查看不过理由</el-button>
 						</template>
 					</template>
 				</el-table-column>
@@ -45,6 +45,107 @@
 			<el-pagination :page-size="10" layout="total, prev, pager, next, jumper" :total="126">
 			</el-pagination>
 		</ContentWarp>
+
+		<!--新增窗口-->
+		<el-dialog title="新增模板" width='600px' center :visible.sync='addInfo.isOpen' :show-close='false'>
+			<el-form :model="addInfo.form" ref='formByAdd' :rules="rules" label-width="80px">
+				<el-form-item label="所属公司" prop='companyCode'>
+					<SelectCompany v-model="addInfo.form.companyCode"></SelectCompany>
+					<div class="disNone">
+						<el-input type='text' v-model="addInfo.form.companyCode"></el-input>
+					</div>
+				</el-form-item>
+				<el-form-item label="产品">
+					<el-input type='text' value='微农贷' disabled></el-input>
+				</el-form-item>
+				<el-form-item label="场景" prop='sceneId'>
+					<el-select v-model='addInfo.form.sceneId' placeholder="请选择" clearable>
+						<el-option v-for='(option,index) of sceneList' :key='index+1' :label='option' :value='index+1'></el-option>
+					</el-select>
+				</el-form-item>
+				<el-form-item label="扩展字段" prop='senceExt'>
+					<el-input type='text' v-model="addInfo.form.senceExt"></el-input>
+				</el-form-item>
+				<el-form-item label="备注" prop='remark'>
+					<el-input type='text' v-model="addInfo.form.remark"></el-input>
+				</el-form-item>
+				<el-form-item label="发送方式" prop='sendType'>
+					<el-radio-group v-model="addInfo.form.sendType">
+						<el-radio :label="1">短信</el-radio>
+					</el-radio-group>
+				</el-form-item>
+				<el-form-item label="发送对象" prop='targetType'>
+					<el-radio-group v-model="addInfo.form.targetType">
+						<el-radio :label="1">借款人</el-radio>
+					</el-radio-group>
+				</el-form-item>
+				<el-form-item label="短信签名" prop='msgSign'>
+					<el-input type='text' v-model="addInfo.form.msgSign"></el-input>
+				</el-form-item>
+				<el-form-item label="短信模板" prop='templateContent'>
+					<el-input type='textarea' v-model="addInfo.form.templateContent"></el-input>
+				</el-form-item>
+			</el-form>
+			<div slot="footer" class="dialog-footer">
+				<el-button type="primary" @click="sendAddAjax('formByAdd')">新 增</el-button>
+				<el-button @click="resetAddInfo">取 消</el-button>
+			</div>
+		</el-dialog>
+		
+		<!--修改窗口-->
+		<el-dialog title="修改模板" width='600px' center :visible.sync='changeInfo.isOpen' :show-close='false'>
+			<el-form :model="changeInfo.form" ref='formByChange' :rules="rules" label-width="80px">
+				<el-form-item label="所属公司" prop='companyCode'>
+					<SelectCompany v-model="changeInfo.form.companyCode"></SelectCompany>
+					<div class="disNone">
+						<el-input type='text' v-model="changeInfo.form.companyCode"></el-input>
+					</div>
+				</el-form-item>
+				<el-form-item label="产品">
+					<el-input type='text' value='微农贷' disabled></el-input>
+				</el-form-item>
+				<el-form-item label="场景" prop='sceneId'>
+					<el-select v-model='changeInfo.form.sceneId' placeholder="请选择" clearable>
+						<el-option v-for='(option,index) of sceneList' :key='index+1' :label='option' :value='index+1'></el-option>
+					</el-select>
+				</el-form-item>
+				<el-form-item label="扩展字段" prop='senceExt'>
+					<el-input type='text' v-model="changeInfo.form.senceExt"></el-input>
+				</el-form-item>
+				<el-form-item label="备注" prop='remark'>
+					<el-input type='text' v-model="changeInfo.form.remark"></el-input>
+				</el-form-item>
+				<el-form-item label="发送方式" prop='sendType'>
+					<el-radio-group v-model="changeInfo.form.sendType">
+						<el-radio :label="1">短信</el-radio>
+					</el-radio-group>
+				</el-form-item>
+				<el-form-item label="发送对象" prop='targetType'>
+					<el-radio-group v-model="changeInfo.form.targetType">
+						<el-radio :label="1">借款人</el-radio>
+					</el-radio-group>
+				</el-form-item>
+				<el-form-item label="短信签名" prop='msgSign'>
+					<el-input type='text' v-model="changeInfo.form.msgSign"></el-input>
+				</el-form-item>
+				<el-form-item label="短信模板" prop='templateContent'>
+					<el-input type='textarea' v-model="changeInfo.form.templateContent"></el-input>
+				</el-form-item>
+			</el-form>
+			<div slot="footer" class="dialog-footer">
+				<el-button type="primary" @click="sendChangeAjax('formByChange')">修 改</el-button>
+				<el-button @click="resetChangeInfo">取 消</el-button>
+			</div>
+		</el-dialog>
+		
+		<!-- 查看不过理由 -->
+		<el-dialog title="不过理由" width='600px' center :visible.sync='reasonInfo.isOpen' :show-close='false'>
+			<p class="text-center">{{reasonInfo.form.verifyFailReason}}</p>
+			<div slot="footer" class="dialog-footer">
+				<el-button type="primary" @click="reasonInfo.isOpen = false">关 闭</el-button>
+			</div>
+		</el-dialog>
+		
 	</el-main>
 </template>
 
--- src/view/sms/config.js
+++ src/view/sms/config.js
@@ -1,4 +1,4 @@
-import listData from '@/data/list/sms-common.json';
+import listData from '@/data/list/sms-config.json';
 
 export default {
 	name: 'SmsConfig',
@@ -8,19 +8,73 @@ export default {
 				list: [],
 				form: {
 					name: '',
-					status:'',
-					sceneId:''
+					status: '',
+					sceneId: ''
 				}
 			},
-			sceneList:['逾期'],
-			statusList:['审核中','已审核','审核不通过','启用','禁用'],
+			sceneList: ['逾期'],
+			statusList: ['审核中', '已审核', '审核不通过', '启用', '禁用'],
 			addInfo: { //存放新增窗口显示状态及它的表单数据
 				isOpen: false,
 				form: {
 					name: '',
-					sceneId:1
+					sceneId: 1,
+					companyCode: '',
+					msgSign: '',
+					platCode: 1,
+					platName: '金爱农',
+					productCode: 'WNA',
+					remark: '',
+					status: 1,
+					senceExt: '',
+					sceneId: 1,
+					sendType: 1,
+					targetType: 1,
+					templateContent: '',
+					templateName: '',
 				}
 			},
+			changeInfo: {
+				isOpen: false,
+				form: {}
+			},
+			reasonInfo: {
+				isOpen: false,
+				form: {}
+			},
+			rules: {
+				companyCode: [{
+					required: true,
+					message: '请选择公司',
+					trigger: 'change'
+				}],
+				sceneId: [{
+					type: 'number',
+					required: true,
+					message: '请选择场景',
+					trigger: 'change'
+				}],
+				senceExt: [{
+					required: true,
+					message: '扩展字段不能为空',
+					trigger: 'change'
+				}],
+				remark: [{
+					required: true,
+					message: '备注不能为空',
+					trigger: 'change'
+				}],
+				msgSign: [{
+					required: true,
+					message: '短信签名不能为空',
+					trigger: 'change'
+				}],
+				templateContent: [{
+					required: true,
+					message: '短信模板不能为空',
+					trigger: 'change'
+				}]
+			}
 		}
 	},
 	methods: {
@@ -34,11 +88,83 @@ export default {
 		formatValue(row, column, cellValue) {
 			//根据传入的prop值确认规则列表
 			if(column.property === 'status') {
-				return this.statusList[cellValue-1]
+				return this.statusList[cellValue - 1]
 			} else if(column.property === 'sceneId') {
-				return this.sceneList[cellValue-1]
+				return this.sceneList[cellValue - 1]
 			}
 		},
+		//新增ajax
+		sendAddAjax(formName) {
+			this.$refs[formName].validate((valid) => {
+				//字段验证是否成功
+				if(valid) {
+					console.log('在此发送addInfo.form数据')
+					console.log(this.addInfo.form)
+				} else {
+					return false;
+				}
+			})
+		},
+		//重置新值数据-关闭窗口
+		resetAddInfo() {
+			//清空表单
+			this.resetForm('formByAdd')
+			//关闭窗口
+			this.addInfo.isOpen = false
+		},
+		//打开修改窗口
+		openChange(obj) {
+			this.changeInfo.isOpen = true;
+			setTimeout(() => {
+				//去掉表单验证处带颜色的边框先
+				this.$refs['formByChange'].resetFields()
+				//提取列表中的值
+				this.changeInfo.form = {
+					id: obj.id,
+					name: obj.name,
+					sceneId: obj.sceneId,
+					companyCode: obj.companyCode,
+					msgSign: obj.msgSign,
+					platCode: obj.platCode,
+					platName: obj.platName,
+					productCode: obj.productCode,
+					remark: obj.remark,
+					status: obj.status,
+					senceExt: obj.senceExt,
+					sceneId: obj.sceneId,
+					sendType: obj.sendType,
+					targetType: obj.targetType,
+					templateContent: obj.templateContent,
+					templateName: obj.templateName
+				}
+			}, 100)
+		},
+		//修改ajax
+		sendChangeAjax(formName) {
+			this.$refs[formName].validate((valid) => {
+				//字段验证是否成功
+				if(valid) {
+					console.log('在此发送changeInfo.form数据')
+					console.log(this.changeInfo.form)
+				} else {
+					return false;
+				}
+			})
+		},
+		//重置修改窗口
+		resetChangeInfo() {
+			//清空表单
+			this.$refs['formByChange'].resetFields()
+			//关闭窗口
+			this.changeInfo.isOpen = false
+		},
+		//打开不过理由
+		openReason(obj) {
+			this.reasonInfo = {
+				isOpen: true,
+				form: obj
+			}
+		}
 	},
 	mounted() {
 		//查询默认列表
