03698e9fbf681bd1264c534fdfd4bb136a97860f
jingjingke <919948650@qq.com>
2017-12-08 15:42:31 +0800

    资料配置相关弹窗

--- src/view/product/Material.vue
+++ src/view/product/Material.vue
@@ -38,6 +38,110 @@
 				</el-col>
 			</el-row>
 		</ContentWarp>
+		
+		<!--新增窗口-->
+		<el-dialog title="新增资料" width='600px' center :visible.sync='addInfo.isOpen' :show-close='false'>
+			<el-form :model="addInfo.form" ref='formByAdd' :rules="rules" label-width="120px">
+				<el-form-item label="资料名称" prop='name'>
+					<el-input v-model="addInfo.form.name"></el-input>
+				</el-form-item>
+				<el-form-item label="排序" prop='seq'>
+					<el-input-number v-model="addInfo.form.seq" :min="0" :max="100"></el-input-number>
+				</el-form-item>
+				<el-form-item label="有数据时才显示" prop='isShow'>
+					<el-radio-group v-model="addInfo.form.isShow">
+						<el-radio :label="1">是</el-radio>
+						<el-radio :label="0">否</el-radio>
+					</el-radio-group>
+				</el-form-item>
+				<el-form-item label="是否最终节点" prop='isFinish'>
+					<el-radio-group v-model="addInfo.form.isFinish">
+						<el-radio :label="1">是</el-radio>
+						<el-radio :label="0">否</el-radio>
+					</el-radio-group>
+				</el-form-item>
+				<template v-if='addInfo.form.isFinish === 1'>
+					<el-form-item label="图标URL" prop='uri'>
+						<el-input v-model="addInfo.form.uri"></el-input>
+					</el-form-item>
+					<el-form-item label="模板ID" prop='templateId'>
+						<el-input v-model="addInfo.form.templateId"></el-input>
+					</el-form-item>
+					<el-form-item label="是否必填" prop='isMust'>
+						<el-radio-group v-model="addInfo.form.isMust">
+							<el-radio :label="0">是</el-radio>
+							<el-radio :label="1">否</el-radio>
+						</el-radio-group>
+					</el-form-item>
+					<template v-if='addInfo.form.isMust===0'>
+						<el-form-item label="资料等级" prop='hideLevel'>
+							<el-input-number v-model="addInfo.form.hideLevel" :min="1" :max="10"></el-input-number>
+						</el-form-item>
+					</template>
+				</template>
+			</el-form>
+			<div slot="footer" class="dialog-footer">
+				<el-button type="primary" @click="sendAddAjax('formByAdd')">新 增</el-button>
+				<el-button @click="resetAddInfo">取 消</el-button>
+			</div>
+		</el-dialog>
+		
+		<!--修改窗口-->
+		<el-dialog title="修改资料" width='600px' center :visible.sync='changeInfo.isOpen' :show-close='false'>
+			<el-form :model="changeInfo.form" ref='formByChange' :rules="rules" label-width="120px">
+				<el-form-item label="资料名称" prop='name'>
+					<el-input v-model="changeInfo.form.name"></el-input>
+				</el-form-item>
+				<el-form-item label="排序" prop='seq'>
+					<el-input-number v-model="changeInfo.form.seq" :min="0" :max="100"></el-input-number>
+				</el-form-item>
+				<el-form-item label="有数据时才显示" prop='isShow'>
+					<el-radio-group v-model="changeInfo.form.isShow">
+						<el-radio :label="1">是</el-radio>
+						<el-radio :label="0">否</el-radio>
+					</el-radio-group>
+				</el-form-item>
+				<el-form-item label="是否最终节点" prop='isFinish'>
+					<el-radio-group v-model="changeInfo.form.isFinish">
+						<el-radio :label="1">是</el-radio>
+						<el-radio :label="0">否</el-radio>
+					</el-radio-group>
+				</el-form-item>
+				<template v-if='changeInfo.form.isFinish === 1'>
+					<el-form-item label="图标URL" prop='uri'>
+						<el-input v-model="changeInfo.form.uri"></el-input>
+					</el-form-item>
+					<el-form-item label="模板ID" prop='templateId'>
+						<el-input v-model="changeInfo.form.templateId"></el-input>
+					</el-form-item>
+					<el-form-item label="是否必填" prop='isMust'>
+						<el-radio-group v-model="changeInfo.form.isMust">
+							<el-radio :label="0">是</el-radio>
+							<el-radio :label="1">否</el-radio>
+						</el-radio-group>
+					</el-form-item>
+					<template v-if='changeInfo.form.isMust===0'>
+						<el-form-item label="资料等级" prop='hideLevel'>
+							<el-input-number v-model="changeInfo.form.hideLevel" :min="1" :max="10"></el-input-number>
+						</el-form-item>
+					</template>
+				</template>
+			</el-form>
+			<div slot="footer" class="dialog-footer">
+				<el-button type="primary" @click="sendChangeAjax('formByChange')">修 改</el-button>
+				<el-button @click="resetChangeInfo">取 消</el-button>
+			</div>
+		</el-dialog>
+		
+		<!--删除按钮-->
+		<el-dialog title="删除" width='600px' center :visible.sync='deleteInfo.isOpen' :show-close='false'>
+			<p class="text-center">确定删除<span class="text-red"> {{deleteInfo.form.name}} </span>吗？</p>
+			<div slot="footer" class="dialog-footer">
+				<el-button type="primary" @click="sendDeleteAjax">确 认</el-button>
+				<el-button @click="deleteInfo.isOpen = false">取 消</el-button>
+			</div>
+		</el-dialog>
+		
 	</el-main>
 </template>
 
--- src/view/product/material.js
+++ src/view/product/material.js
@@ -9,6 +9,44 @@ export default {
 				title:'',
 				data:[],
 				isFinish:false,
+			},
+			addInfo: { //存放新增窗口显示状态及它的表单数据
+				isOpen: false,
+				form: {
+					name: '',
+					seq: 0,
+					isShow:1,
+					isFinish:0,
+					uri:'',
+					templateId:'',
+					isMust:1,
+					hideLevel:1
+				}
+			},
+			changeInfo: {
+				isOpen: false,
+				form: {}
+			},
+			deleteInfo: {
+				isOpen: false,
+				form: {}
+			},
+			rules: {
+				name: [{
+					required: true,
+					message: '资料名称不能为空',
+					trigger: 'change'
+				}],
+				uri: [{
+					required: true,
+					message: '图标URL不能为空',
+					trigger: 'change'
+				}],
+				templateId: [{
+					required: true,
+					message: '模板ID不能为空',
+					trigger: 'change'
+				}]
 			}
 		}
 	},
@@ -31,6 +69,75 @@ export default {
 				return cellValue===0?'否':'是';
 			}
 		},
+		//新增ajax
+		sendAddAjax(formName) {
+			this.$refs[formName].validate((valid) => {
+				//字段验证是否成功
+				if(valid) {
+					console.log('在此发送addInfo.form数据')
+					console.log(this.addInfo.form)
+				} else {
+					return false;
+				}
+			})
+		},
+		//重置新值数据-关闭窗口
+		resetAddInfo() {
+			//清空表单
+			this.resetForm('formByAdd')
+			//关闭窗口
+			this.addInfo.isOpen = false
+		},
+		//打开修改窗口
+		openChange(obj) {
+			this.changeInfo.isOpen = true;
+			setTimeout(() => {
+				//去掉表单验证处带颜色的边框先
+				this.$refs['formByChange'].resetFields()
+				//提取列表中的值
+				this.changeInfo.form = {
+					id:obj.id,
+					name: obj.name,
+					seq: obj.seq,
+					isShow:obj.isShow,
+					isFinish:obj.isFinish,
+					uri:obj.uri || '',
+					templateId:obj.templateId || '',
+					isMust:obj.isMust || 1,
+					hideLevel:obj.hideLevel || 1
+				}
+			}, 100)
+		},
+		//修改ajax
+		sendChangeAjax(formName) {
+			this.$refs[formName].validate((valid) => {
+				//字段验证是否成功
+				if(valid) {
+					console.log('在此发送changeInfo.form数据')
+					console.log(this.changeInfo.form)
+				} else {
+					return false;
+				}
+			})
+		},
+		//重置修改窗口
+		resetChangeInfo() {
+			//清空表单
+			this.$refs['formByChange'].resetFields()
+			//关闭窗口
+			this.changeInfo.isOpen = false
+		},
+		//打开删除窗口
+		openDelete(obj) {
+			this.deleteInfo = {
+				isOpen: true,
+				form: obj
+			}
+		},
+		//删除ajax
+		sendDeleteAjax() {
+			console.log('确认删除ID为'+this.deleteInfo.form.id+'的资料')
+		}
 	},
 	mounted(){
 		this.list.title = materialTree[0].name;
